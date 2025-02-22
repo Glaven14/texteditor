75c9d57de155400cb2508cb609c68b8701d5c6db
Ian Gunn <23ian.gunn@gmail.com>
2025-02-22 00:37:27 -0600

    Initial naive implementation of SimpleStringBuffer

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -1,39 +1,86 @@
 package edu.grinnell.csc207.texteditor;
 
+import java.lang.String;
+
 /**
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
+    
+    private int size;
+
+    private String cursor;
+    
+    private int cursorPos;
+
+    private String buffer;
+
+
+    public SimpleStringBuffer () {
+        size = 0;
+        cursor = Character.toString('\u25AE'); 
+        cursorPos = 0;
+        buffer = "" + cursor;
+    }
+
+    /**
+     * @param ch
+     */
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        String front;
+        String back;
+        String cha = Character.toString(ch);
+        front = buffer.substring(0, cursorPos);
+        back = buffer.substring(cursorPos + 1);
+        buffer = front.concat(cha).concat(cursor).concat(back);
+        cursorPos++;
+        size++;
     }
 
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        //add empty buffer case.
+        String front;
+        String back;
+        front = buffer.substring(0, cursorPos - 1);
+        back = buffer.substring(cursorPos + 1);
+        buffer = front.concat(cursor).concat(back);
+        cursorPos--;
+        size--;
     }
 
-    public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
-    }
+    public int getCursorPosition() {return cursorPos;}
 
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        // throw in an if to check if at start of buffer.
+        String front;
+        String movingCh;
+        String back;
+        front = buffer.substring(0, cursorPos - 1); // Grabs the characters before the one swapping pos with cursor.
+        movingCh =  buffer.substring(cursorPos - 1, cursorPos); // Grabs the character swapping pos with cursor
+        back =  buffer.substring(cursorPos + 1); // Grabs the remaining chararcters after the cursor.
+        buffer = front.concat(cursor).concat(movingCh).concat(back);
+        cursorPos--;
     }
 
-    public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+    public void moveRight() {  
+        // throw in an if to check if at end of buffer.
+        String front;
+        String movingCh;
+        String back;
+        front = buffer.substring(0, cursorPos); // Grabs the characters before the cursor.
+        movingCh =  buffer.substring(cursorPos + 1, cursorPos + 2); // Grabs the character swapping pos with cursor
+        back =  buffer.substring(cursorPos + 2); // Grabs the remaining chararcters after the cursor.
+        buffer = front.concat(movingCh).concat(cursor).concat(back);
+        cursorPos++;
     }
 
-    public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
-    }
+    public int getSize() {return size;}
 
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        //check for index validity
+        return buffer.charAt(i);
     }
 
     @Override
-    public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
-    }
+    public String toString() {return buffer;}
 }
