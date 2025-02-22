88741ddadfe5b2ced15920c5c49e57544d264f2c
Ian Gunn <23ian.gunn@gmail.com>
2025-02-22 17:03:53 -0600

    Completed SimpleStringBuffer and Testing suite

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -16,9 +16,9 @@ public class SimpleStringBuffer {
     private String buffer;
 
 
-    public SimpleStringBuffer () {
+    public SimpleStringBuffer() {
         size = 0;
-        cursor = Character.toString('\u25AE'); 
+        cursor = Character.toString('\u25AE');
         cursorPos = 0;
         buffer = "" + cursor;
     }
@@ -37,47 +37,73 @@ public class SimpleStringBuffer {
         size++;
     }
 
+    /**
+     * 
+     */
     public void delete() {
-        //add empty buffer case.
-        String front;
-        String back;
-        front = buffer.substring(0, cursorPos - 1);
-        back = buffer.substring(cursorPos + 1);
-        buffer = front.concat(cursor).concat(back);
-        cursorPos--;
-        size--;
+        if (this.size > 0) {
+            String front;
+            String back;
+            front = buffer.substring(0, cursorPos - 1);
+            back = buffer.substring(cursorPos + 1);
+            buffer = front.concat(cursor).concat(back);
+            cursorPos--;
+            size--;
+        }
     }
 
+    /**
+     * 
+     * @return
+     */
     public int getCursorPosition() {return cursorPos;}
 
+    /**
+     * 
+     */
     public void moveLeft() {
-        // throw in an if to check if at start of buffer.
-        String front;
-        String movingCh;
-        String back;
-        front = buffer.substring(0, cursorPos - 1); // Grabs the characters before the one swapping pos with cursor.
-        movingCh =  buffer.substring(cursorPos - 1, cursorPos); // Grabs the character swapping pos with cursor
-        back =  buffer.substring(cursorPos + 1); // Grabs the remaining chararcters after the cursor.
-        buffer = front.concat(cursor).concat(movingCh).concat(back);
-        cursorPos--;
+        if (this.cursorPos > 0) {
+            String front;
+            String movingCh;
+            String back;
+            front = buffer.substring(0, cursorPos - 1); // Grabs the characters before the one swapping pos with cursor.
+            movingCh =  buffer.substring(cursorPos - 1, cursorPos); // Grabs the character swapping pos with cursor
+            back =  buffer.substring(cursorPos + 1); // Grabs the remaining chararcters after the cursor.
+            buffer = front.concat(cursor).concat(movingCh).concat(back);
+            cursorPos--;
+        }
     }
 
+    /**
+     * 
+     */
     public void moveRight() {  
-        // throw in an if to check if at end of buffer.
-        String front;
-        String movingCh;
-        String back;
-        front = buffer.substring(0, cursorPos); // Grabs the characters before the cursor.
-        movingCh =  buffer.substring(cursorPos + 1, cursorPos + 2); // Grabs the character swapping pos with cursor
-        back =  buffer.substring(cursorPos + 2); // Grabs the remaining chararcters after the cursor.
-        buffer = front.concat(movingCh).concat(cursor).concat(back);
-        cursorPos++;
+        if (this.cursorPos < this.size) {
+            String front;
+            String movingCh;
+            String back;
+            front = buffer.substring(0, cursorPos); // Grabs the characters before the cursor.
+            movingCh =  buffer.substring(cursorPos + 1, cursorPos + 2); // Grabs the character swapping pos with cursor
+            back =  buffer.substring(cursorPos + 2); // Grabs the remaining chararcters after the cursor.
+            buffer = front.concat(movingCh).concat(cursor).concat(back);
+            cursorPos++;
+        }
     }
 
+    /**
+     * 
+     * @return
+     */
     public int getSize() {return size;}
 
+    /**
+     * @param i
+     * @return
+     */
     public char getChar(int i) {
-        //check for index validity
+        if (i < 0 || i > size) {
+            throw new IllegalArgumentException("index is outside the bounds of String");
+        }
         return buffer.charAt(i);
     }
 
--- src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
@@ -1,5 +1,87 @@
 package edu.grinnell.csc207.texteditor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+
 public class SimpleStringBufferTests {
-    /** TODO: fill me in with unit and property tests! */
+    
+    @Test
+    public void addingAndDeleting() {
+        SimpleStringBuffer b = new SimpleStringBuffer();
+        b.insert('h');
+        b.insert('e');
+        assertEquals("" + 'h' + 'e' + '\u25AE', b.toString());
+        b.delete();
+        assertEquals("" + 'h' + '\u25AE', b.toString());        
+    } 
+
+    @Test
+    public void movingCursor() {
+        SimpleStringBuffer b = new SimpleStringBuffer();
+        b.insert('h');
+        b.insert('e');
+        b.insert('l');
+        b.insert('l');
+        b.insert('o');
+        b.moveLeft();
+        assertEquals("" + 'h' + 'e' + 'l' + 'l' + '\u25AE' + 'o', b.toString());
+        b.moveRight();
+        assertEquals("" + 'h' + 'e' + 'l' + 'l' + 'o' + '\u25AE', b.toString());
+        assertEquals(5, b.getCursorPosition());
+    }
+
+    @Test
+    public void getSizeAndChar() {
+        SimpleStringBuffer b = new SimpleStringBuffer();
+        assertEquals(0, b.getSize());
+        b.insert('h');
+        b.insert('e');
+        b.insert('l');
+        b.insert('l');
+        b.insert('o');
+        assertEquals(5, b.getSize());
+        assertEquals('e', b.getChar(1));
+    }
+
+    @Test
+    public void deletingEdgeCase() {
+        SimpleStringBuffer b = new SimpleStringBuffer();
+        b.delete();
+        assertEquals("" + '\u25AE', b.toString());
+        b.insert('h');
+        b.insert('e');
+        b.delete();
+        b.delete();
+        b.delete();
+        assertEquals("" + '\u25AE', b.toString());   
+    } 
+
+    @Test
+    public void movingCursorEdgeCase() {
+        SimpleStringBuffer b = new SimpleStringBuffer();
+        assertEquals(0, b.getCursorPosition());
+        b.moveLeft();
+        assertEquals("" + '\u25AE', b.toString());
+        b.insert('h');
+        b.insert('e');
+        b.insert('l');
+        b.insert('l');
+        b.insert('o');
+        b.moveRight();
+        assertEquals("" + 'h' + 'e' + 'l' + 'l' + 'o' + '\u25AE', b.toString());
+    }
+
+    @Property
+    public boolean insertingManyChar(@ForAll @IntRange(min = 0, max = 20) int sz, @ForAll char ch) {
+        SimpleStringBuffer b = new SimpleStringBuffer();
+        for (int i = 0; i < sz; i++) {
+            b.insert(ch);
+        }
+        return (sz == b.getSize());
+    }
 }
